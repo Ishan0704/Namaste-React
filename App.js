4863313b23fd8c3eb6bf14ab3287eaf505d0505d
Ishan0704 <ishan2003dixit@gmail.com>
2024-07-12 20:13:41 +0530

    component composite done

--- App.js
+++ App.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const heading= React.createElement("h1",{id:123},"Hii there"); // CDN REACT
+const heading= React.createElement("h1",{id:123},"Hii there"); // react element
+
+const JsxHeading1 = () => (
+    <div>
+        <h1>This is jsx trial</h1>
+        <JsxHeading2/>
+    </div>
+); // functional component
+
+const JsxHeading2 = () => (
+    <h2>This is another text</h2>
+); // functional component
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<JsxHeading1/>);
