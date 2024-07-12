bcae04f79c235afc3a7be4922c6abd2f4ca9eecb
Ishan0704 <ishan2003dixit@gmail.com>
2024-07-12 19:59:35 +0530

    trial

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
 
-const heading= React.createElement("h1",{id:123},"Hii there");
+const heading= React.createElement("h1",{id:123},"Hii there"); // CDN REACT
 
-const JsxElemnt = <h1>Hello this my first jsx trial</h1>; 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
-root.appendChild(JsxElement);
