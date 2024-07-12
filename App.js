9bd52e93bcf53dd1bd8d2df522dc1bb8c0ba5592
Ishan0704 <ishan2003dixit@gmail.com>
2024-07-12 19:58:45 +0530

    trial

---
+++ App.js
@@ -0,0 +1,7 @@
+
+const heading= React.createElement("h1",{id:123},"Hii there");
+
+const JsxElemnt = <h1>Hello this my first jsx trial</h1>; 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+root.appendChild(JsxElement);
