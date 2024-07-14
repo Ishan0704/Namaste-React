112d3e7aa88941c3896b13286fa92fb705855d60
Ishan0704 <ishan2003dixit@gmail.com>
2024-07-14 18:41:03 +0530

    written propes properly

--- App.js
+++ App.js
@@ -19,19 +19,20 @@ const Header = ()=>{
 }
 
 const Card = (props) => {
+
+    const {name, rating, cunnines, Avgtime} = props;
+    
     return(
         <div className='card'>
             <div className="card__image">
                 <img src="" alt="cardImage" />
             </div>
-            <div className='card__title'>{props.title}</div>
-            <div className="card__description">{props.descrption}</div>
+            <div className='card__title'>{name}</div>
+            <div className="card__description">{cunnines}</div>
             <div className="card__deatils">
-                <div className='card__common card__ratings'>{props.ratings}</div>
-                <div className="card__common card__time">{props.time} Minutes</div>
+                <div className='card__common card__ratings'>{rating}</div>
+                <div className="card__common card__time">{Avgtime} Minutes</div>
             </div>  
-            
-
         </div>
     )
 }
@@ -41,14 +42,13 @@ const Body = () =>{
         <div className='body'>
             <div className="search">Search</div>
             <div className="card__container">
-                <Card title="Dominos" descrption="We deliver best pizza" ratings='4.5' time='30-40'/>
-                <Card title="KFC" descrption="Chicked Basket" ratings='4.7' time='20-30'/>
-                <Card title="Pizza Hut" descrption="Best Pan Pizzas" ratings='4.2' time='30-40'/>
+                <Card name="Dominos" cunnines="We deliver best pizza" rating='4.5' Avgtime='30-40'/>
+                <Card name="KFC" cunnines="Chicked Basket" rating='4.7' Avgtime='20-30'/>
+                <Card name="Pizza Hut" cunnines="Best Pan Pizzas" rating='4.2' Avgtime='30-40'/>
             </div>
         </div>
     )
 }
-
 const App = () =>{
     return(
         <div>
