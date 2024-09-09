4840b59d9457dfa8094807d9d0dc972ae883ed5f
Ishan0704 <ishan2003dixit@gmail.com>
2024-09-09 12:35:55 +0530

    console.log(data)

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -1,14 +1,35 @@
 import './body.css';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom/client';
 import { restaurantList } from '../../assets/RestData';
 import Card from '../Card';
+import { useEffect } from 'react';
+
+
 
 const Body = () =>{
+
+    const [List, setlist]=useState(restaurantList);
+
+    useEffect(  
+        ()=>{
+            datafunction()
+        },[]
+    )
+
+    const datafunction=async()=>{
+       const orgData = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.9974533&lng=73.78980229999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+       const json = await orgData.json();
+       console.log(json);
+       
+    }
+
     return (
         <div className='body'>
             <div className="search">Search</div>
             <div className="card__container">
                 {
-                    restaurantList.map((res) => <Card key={res.data.id} Inner_data={res}/>)
+                    List.map((res) => <Card key={res.data.id} Inner_data={res}/>)
                 }
             </div>
         </div>
